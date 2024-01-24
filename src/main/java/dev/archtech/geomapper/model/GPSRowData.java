61d24ca5b1ef9b79d5ead4d40ce24eec920a2d35
Jake <jake.arsenault@hotmail.ca>
2024-01-24 15:31:02 -0500

    Remove Lombok

--- src/main/java/dev/archtech/geomapper/model/GPSRowData.java
+++ src/main/java/dev/archtech/geomapper/model/GPSRowData.java
@@ -1,11 +1,32 @@
 package dev.archtech.geomapper.model;
 
 import dev.archtech.geomapper.model.map.Coordinates;
-import lombok.Data;
-
-@Data
 public class GPSRowData {
     private int index;
     private String time;
     private Coordinates coordinates;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
 }
--- src/main/java/dev/archtech/geomapper/model/map/Secret.java
+++ src/main/java/dev/archtech/geomapper/model/map/Secret.java
@@ -1,9 +1,22 @@
 package dev.archtech.geomapper.model.map;
 
-import lombok.Data;
-
-@Data
 public class Secret {
     private String apiKey;
     private String signature;
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
 }
